d5db2a0b11e66bb533612978ac512857b00c631b
Steve Sperandeo <steve.sperandeo@gmail.com>
2012-10-11 17:44:27 -0700

    Working, basic string class.

--- Utf8String.cc
+++ Utf8String.cc
@@ -4,59 +4,43 @@
 #include <cstring>
 #include <string>
 #include <algorithm>
-
+#include <utility>
 #include <iostream>
 
 
 namespace jet{
 
+    #define Utf8StringDefaultValues include_bom(true),number_of_characters(0),size_of_character_data(0),characters(NULL)
 
-    Utf8String::Utf8String():
-        include_bom( true ),
-        number_of_characters( 0 ),
-        size_of_character_data( 0 ),
-        characters( NULL )
-    {
+
+    Utf8String::Utf8String(): Utf8StringDefaultValues{
 
     }
 
 
-    Utf8String::Utf8String( const Utf8String& other ){
+    Utf8String::Utf8String( const Utf8String& other ): Utf8StringDefaultValues{
 
-        //use the default constructor to build this
-        Utf8String *temp_string = new Utf8String( "abc", 3 );
-            swap( *this, *temp_string );
-            delete temp_string;
+        this->include_bom = other.include_bom;
+        this->number_of_characters = other.number_of_characters;
+        this->size_of_character_data = other.size_of_character_data;
 
-        //copy the elements
-            //this->include_bom = other.include_bom;
-            //this->number_of_characters = other.number_of_characters;
-            //this->size_of_character_data = other.size_of_character_data;
-
-            /*
-            if( this->size_of_character_data > 0 ){
-                this->characters = new char[ this->size_of_character_data ];
-                memcpy( this->characters, other.characters, this->size_of_character_data );
-            }
-           */
+        if( this->size_of_character_data > 0 ){
+            this->characters = new char[ this->size_of_character_data ];
+            memcpy( this->characters, other.characters, this->size_of_character_data );
+        }
 
     }
 
 
-    Utf8String::Utf8String( Utf8String&& other ){
 
-        //use the default constructor to build this
-            Utf8String *temp_string = new Utf8String;
-            swap( *this, *temp_string );
-            delete temp_string;
+    Utf8String::Utf8String( Utf8String&& other ): Utf8StringDefaultValues{
 
-        //steal the contents of "other"
-            swap( *this, other );
+        *this = std::move( other );
 
     }
 
 
-    Utf8String::Utf8String( const char *source_string, size_t size_in_bytes ){
+    Utf8String::Utf8String( const char *source_string, size_t size_in_bytes ): Utf8StringDefaultValues{
 
         if( size_in_bytes == 0 ){
             throw Exception( "Utf8String size cannot be zero." );
@@ -71,7 +55,7 @@ namespace jet{
     }
 
 
-    Utf8String::Utf8String( const std::string &source_string ){
+    Utf8String::Utf8String( const std::string &source_string ): Utf8StringDefaultValues{
 
         const char *string_data = source_string.c_str();
 
@@ -94,6 +78,25 @@ namespace jet{
         return *this;
 
     }
+
+
+    Utf8String& Utf8String::operator=( Utf8String &&other ){
+
+        //create an empty string and replace *this
+            Utf8String *temp = new Utf8String;
+            swap( *this, *temp );
+            delete temp; //frees the existing *this
+
+        //steal the contents of "other" and set "other" to the empty string
+            swap( *this, other );
+
+        //now "other" should be the blank string
+            return *this;
+
+    }
+
+
+
 
 
     Utf8String::~Utf8String(){
@@ -133,10 +136,6 @@ namespace jet{
     }
 
 
-
-
-
-
     void swap( Utf8String& first, Utf8String& second ){
 
         // enable ADL (not necessary in our case, but good practice)
@@ -148,6 +147,59 @@ namespace jet{
         swap( first.number_of_characters, second.number_of_characters );
         swap( first.size_of_character_data, second.size_of_character_data );
         swap( first.characters, second.characters );
+
+    }
+
+
+    Utf8String& Utf8String::operator+=( const Utf8String &right ){
+
+        //if either is empty, return a copy of the other
+            if( right.number_of_characters == 0 ){
+                return *this;
+            }
+
+            if( this->number_of_characters == 0 ){
+                *this = right;
+                return *this;
+            }
+
+
+        //else, concatenate them into a new third (and swap with *this)
+            size_t new_size_of_character_data = this->size_of_character_data + right.size_of_character_data;
+            size_t new_number_of_characters = this->number_of_characters + right.number_of_characters;
+
+            Utf8String *temp = new Utf8String;
+
+
+            temp->characters = new char[ new_size_of_character_data ];
+
+            memcpy( temp->characters, this->characters, this->size_of_character_data );
+            memcpy( temp->characters + this->size_of_character_data, right.characters, right.size_of_character_data );
+
+            temp->number_of_characters = new_number_of_characters;
+            temp->size_of_character_data = new_size_of_character_data;
+            temp->include_bom = this->include_bom;
+
+            swap( *this, *temp );
+            delete temp;
+
+        return *this;
+
+
+    }
+
+
+    const Utf8String Utf8String::operator+( const Utf8String &right ){
+
+        return Utf8String( *this ) += right;
+
+    }
+
+
+    std::ostream& operator<<( std::ostream &out, const Utf8String &output_string ){
+
+        out.write( output_string.characters, output_string.size_of_character_data );
+        return out;
 
     }
 
