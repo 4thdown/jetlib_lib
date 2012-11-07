9bddf0733450fedb1175f348059aa38a0f060fb4
Steve Sperandeo <steve.sperandeo@gmail.com>
2012-11-06 23:41:25 -0800

    Make Exception work properly.

--- Exception.cc
+++ Exception.cc
@@ -6,9 +6,9 @@
 
 namespace jet{
 
-    Exception::Exception( const char * ){
+    Exception::Exception( const char *exception_message ){
 
-//        /this->message = Utf8String( exception_message, strlen(exception_message) );
+        this->message = Utf8String( exception_message );
 
     }
 
