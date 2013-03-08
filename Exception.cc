7af235edba39ac963df532986cc819daf6d59539
Steve Sperandeo <steve.sperandeo@gmail.com>
2013-03-07 23:59:34 -0800

    Added filename and line number to exception.

--- Exception.cc
+++ Exception.cc
@@ -6,6 +6,7 @@
 
 namespace jet{
 
+
     Exception::Exception( const char *exception_message ){
 
         this->message = Utf8String( exception_message );
@@ -17,5 +18,22 @@ namespace jet{
         this->message = exception_message;
 
     }
+
+    Exception::Exception( const char *exception_message, const char *filename, const int line_number ){
+
+        this->message = Utf8String( exception_message );
+        this->filename = Utf8String( filename );
+        this->line_number = line_number;
+
+    }
+
+    Exception::Exception( const Utf8String exception_message, const char *filename, const int line_number ){
+
+        this->message = exception_message;
+        this->filename = Utf8String( filename );
+        this->line_number = line_number;
+
+    }
+
 
 }
